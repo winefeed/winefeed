884d032ae5354558612a65ceef2fda6727292c0d
Markus Nilsson <markus@esima.se>
2026-01-22 16:02:43 +0100

    Change admin email to markus@vinkoll.se

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -42,7 +42,7 @@ export default function AdminLayout({
     <div className="flex min-h-screen bg-background">
       <Sidebar
         sections={ADMIN_NAVIGATION}
-        userEmail="admin@winefeed.se"
+        userEmail="markus@vinkoll.se"
         userRoles={['ADMIN']}
         isAdmin={true}
       />
